1c5a9d039961c9f34746909044d54bec4896849f
Milan Tarami <milantarami.dev@gmail.com>
2023-02-07 01:02:02 +0545

    [fix] add ref as prop

--- src/App/components/Button/Button.jsx
+++ src/App/components/Button/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MuiButton from "@mui/material/Button";
 
 const Button = React.forwardRef((props, ref) => (
-  <MuiButton variant="outlined" color="primary" {...props} />
+  <MuiButton ref={ref} variant="outlined" color="primary" {...props} />
 ));
 
 export default Button;
