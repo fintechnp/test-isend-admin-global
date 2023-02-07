eb8431b851ce857766af1ce76580a212874f0b73
Milan Tarami <milantarami.dev@gmail.com>
2023-02-07 14:03:59 +0545

    [add] core components

---
+++ src/App/components/Container/ModalGridFormContainer.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+
+const Container = styled(Grid)(({ theme }) => ({
+  maxWidth: "900px",
+  borderRadius: "5px",
+  [theme.breakpoints.up("sm")]: {
+    minWidth: "350px",
+  },
+}));
+
+export default function ModalGridFormContainer({ children }) {
+  return (
+    <Container container columnSpacing={2} rowSpacing={0}>
+      {children}
+    </Container>
+  );
+}
