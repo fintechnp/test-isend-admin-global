cdae0c61529ee45905caf246443b4a74667b67bc
Milan Tarami <milantarami.dev@gmail.com>
2023-02-14 17:07:41 +0545

    [chore] format code

--- src/App/helpers/buildRoute.js
+++ src/App/helpers/buildRoute.js
@@ -1,42 +1,38 @@
 export default function buildRoute(routePath, ...params) {
-  let arrayContainsObject = params.some(
-    (e) => Object.prototype.toString.call(e) === "[object Object]"
-  );
+    let arrayContainsObject = params.some((e) => Object.prototype.toString.call(e) === "[object Object]");
 
-  if (arrayContainsObject && params.length > 1)
-    throw new Error(
-      "Expected only single parameter while route parameter has an object as a parameter"
-    );
+    if (arrayContainsObject && params.length > 1)
+        throw new Error("Expected only single parameter while route parameter has an object as a parameter");
 
-  if (arrayContainsObject) {
-    routePath = routePath.split("/");
+    if (arrayContainsObject) {
+        routePath = routePath.split("/");
 
-    let routeParams = params[0];
+        let routeParams = params[0];
 
-    Object.keys(routeParams).forEach((key) => {
-      routePath = routePath.map((path) => {
-        if (`:${key}` === path) {
-          let value = routeParams[key];
-          delete routeParams[key];
-          return value;
+        Object.keys(routeParams).forEach((key) => {
+            routePath = routePath.map((path) => {
+                if (`:${key}` === path) {
+                    let value = routeParams[key];
+                    delete routeParams[key];
+                    return value;
+                }
+                return path;
+            });
+        });
+
+        routePath = routePath.join("/");
+
+        if (Object.keys(routeParams).length > 0) {
+            routePath = routePath + "?" + new URLSearchParams(routeParams);
         }
-        return path;
-      });
-    });
+    } else {
+        routePath = routePath.replace(/:([^/]+)/, params[0]);
 
-    routePath = routePath.join("/");
+        params.shift();
 
-    if (Object.keys(routeParams).length > 0) {
-      routePath = routePath + "?" + new URLSearchParams(routeParams);
+        if (params.length > 0) {
+            routePath = routePath + "?" + params.join("&");
+        }
     }
-  } else {
-    routePath = routePath.replace(/:([^/]+)/, params[0]);
-
-    params.shift();
-
-    if (params.length > 0) {
-      routePath = routePath + "?" + params.join("&");
-    }
-  }
-  return routePath;
+    return routePath;
 }
