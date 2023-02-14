3d1c11566c6f7c0252542d3fdcd6e0a305b05323
Milan Tarami <milantarami.dev@gmail.com>
2023-02-14 17:05:03 +0545

    [style] style, props for text

--- src/App/components/Button/SubmitButton.jsx
+++ src/App/components/Button/SubmitButton.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
+import CircularProgress from "@mui/material/CircularProgress";
 
 const StyledButton = styled(Button)(({ theme }) => ({
     minWidth: "100px",
@@ -12,26 +13,42 @@ const StyledButton = styled(Button)(({ theme }) => ({
         background: theme.palette.primary.dark,
     },
     textTransform: "capitalize",
+    display: "flex",
+    gap: theme.spacing(1),
+    alignItems: "center",
+    justifyContent: "center",
 }));
 
-const SubmitButton = React.forwardRef(({ isLoading, children, isAddMode, ...rest }, ref) => (
-    <StyledButton
-        ref={ref}
-        type="submit"
-        size="small"
-        variant="contained"
-        disabled={isLoading}
-        disableElevation
-        {...rest}
-    >
-        {children}
-        {!children && (isAddMode ? (!isLoading ? "Submit" : "Submitting") : !isLoading ? "Update" : "Updating")}
-    </StyledButton>
-));
+const SubmitButton = React.forwardRef(
+    ({ isLoading, children, isAddMode, submitText, submittingText, ...rest }, ref) => (
+        <StyledButton
+            ref={ref}
+            type="submit"
+            size="small"
+            variant="contained"
+            disabled={isLoading}
+            disableElevation
+            {...rest}
+        >
+            {children}
+            {!children && (isAddMode ? (!isLoading ? submitText : submittingText) : !isLoading ? "Update" : "Updating")}
+            {isLoading ? <CircularProgress color="inherit" size={10} /> : ""}
+        </StyledButton>
+    ),
+);
 
 export default SubmitButton;
 
 SubmitButton.propTypes = {
     isLoading: PropTypes.bool,
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf([PropTypes.node])]),
+    isAddMode: PropTypes.bool,
+    submitText: PropTypes.string,
+    submittingText: PropTypes.string,
+};
+
+SubmitButton.defaultProps = {
+    isAddMode: true,
+    submitText: "Submit",
+    submittingText: "Submitting",
 };
