5ca3cbade0372fd4f663640359db3d1a3319bc49
Milan Tarami <milantarami.dev@gmail.com>
2023-02-14 17:07:17 +0545

    [fix] add encType support for hook form

--- src/App/core/hook-form/HookForm.jsx
+++ src/App/core/hook-form/HookForm.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import { FormProvider } from "react-hook-form";
 
-export default function HookForm({ children, onSubmit, ...rest }) {
+export default function HookForm({ children, onSubmit, encType, ...rest }) {
     return (
         <FormProvider {...rest}>
-            <form onSubmit={rest?.handleSubmit(onSubmit)}>{children}</form>
+            <form onSubmit={rest?.handleSubmit(onSubmit)} {...{ encType: encType }}>
+                {children}
+            </form>
         </FormProvider>
     );
 }
@@ -13,4 +15,5 @@ export default function HookForm({ children, onSubmit, ...rest }) {
 HookForm.propTypes = {
     children: PropTypes.oneOfType([PropTypes.node, PropTypes.arrayOf(PropTypes.node)]),
     onSubmit: PropTypes.func,
+    enctype: PropTypes.oneOf(["multipart/form-data", "application/x-www-form-urlencoded", "text/plain"]),
 };
