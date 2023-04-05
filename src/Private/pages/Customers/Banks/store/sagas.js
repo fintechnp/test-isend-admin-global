7a5a0aefaa41280a9691e98632a509fd00d71c4e
govind <govindbohara76@gmail.com>
2023-04-05 09:04:13 +0545

    shown bank list

--- src/Private/pages/Customers/Banks/store/sagas.js
+++ src/Private/pages/Customers/Banks/store/sagas.js
@@ -4,21 +4,6 @@ import actions from "./actions";
 
 const api = new Api();
 
-export const getAllBankList = takeEvery(actions.GET_BANK, function* (action) {
-    try {
-        const res = yield call(api.get, ``, action.query);
-        yield put({
-            type: actions.GET_BANK_SUCCESS,
-            response: res,
-        });
-    } catch (error) {
-        yield put({
-            type: actions.GET_BANK_FAILED,
-            error: error?.data,
-        });
-    }
-});
-
 export const getBankDetails = takeEvery(actions.GET_BANK_DETAILS, function* (action) {
     try {
         const res = yield call(api.get, `/customer/${action.id}/bank`, action.query);
@@ -34,57 +19,6 @@ export const getBankDetails = takeEvery(actions.GET_BANK_DETAILS, function* (act
     }
 });
 
-// export const addBank = takeEvery(actions.ADD_BANK, function* (action) {
-//     try {
-//         const res = yield call(api.post, `deliveryoption`, action.data);
-//         yield put({
-//             type: actions.ADD_BANK_SUCCESS,
-//             response: res,
-//         });
-//         yield put({ type: "SET_TOAST_DATA", response: res });
-//     } catch (error) {
-//         yield put({
-//             type: actions.ADD_BANK_FAILED,
-//             error: error.data,
-//         });
-//         yield put({ type: "SET_TOAST_DATA", response: error.data });
-//     }
-// });
-
-// export const updateBank = takeEvery(actions.UPDATE_BANK, function* (action) {
-//     try {
-//         const res = yield call(api.put, `deliveryoption/${action.id}`, action.data);
-//         yield put({
-//             type: actions.UPDATE_BANK_SUCCESS,
-//             response: res,
-//         });
-//         yield put({ type: "SET_TOAST_DATA", response: res });
-//     } catch (error) {
-//         yield put({
-//             type: actions.UPDATE_BANK_FAILED,
-//             error: error.data,
-//         });
-//         yield put({ type: "SET_TOAST_DATA", response: error.data });
-//     }
-// });
-
-// export const deleteBank = takeEvery(actions.DELETE_BANK, function* (action) {
-//     try {
-//         const res = yield call(api.delete, `deliveryoption/${action.id}`);
-//         yield put({
-//             type: actions.DELETE_BANK_SUCCESS,
-//             response: res,
-//         });
-//         yield put({ type: "SET_TOAST_DATA", response: res });
-//     } catch (error) {
-//         yield put({
-//             type: actions.DELETE_BANK_FAILED,
-//             error: error.data,
-//         });
-//         yield put({ type: "SET_TOAST_DATA", response: error.data });
-//     }
-// });
-
 export default function* saga() {
-    yield all([getAllBankList, getBankDetails]);
+    yield all([getBankDetails]);
 }
