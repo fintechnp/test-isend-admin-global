f938ccba52f24a14ebf743aac7989d2edc3d9683
Milan Tarami <milantarami.dev@gmail.com>
2023-06-13 16:45:25 +0545

    [add] default sending country for filter

--- src/App/config/app.js
+++ src/App/config/app.js
@@ -4,6 +4,8 @@ const app = {
     isDevelopmentMode: import.meta.env.MODE === "development",
 
     apiBaseUrl: import.meta.env.REACT_APP_API_BASE_URL,
+
+    defaultSendingCountry: "SGP",
 };
 
 export default app;
