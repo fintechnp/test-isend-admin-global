2e748b0ed201ad57ab1f13cc64bbff8223c4ed83
milanr21 <milanrawal@fintech.com.np>
2023-11-08 11:47:21 +0545

    refactor: Rearrange ledger details items

--- src/Private/pages/B2BTransactions/ViewBatchTransaction.jsx
+++ src/Private/pages/B2BTransactions/ViewBatchTransaction.jsx
@@ -34,6 +34,8 @@ export default function ViewBatchTransaction({ title }) {
 
     const { response, loading } = useSelector((state) => state.get_batch_transaction_by_id);
 
+    console.log(response);
+
     useEffect(() => {
         dispatch(batchTransactionActions.get_batch_transaction(batchTransactionId, filterSchema));
     }, [filterSchema]);
@@ -132,6 +134,13 @@ export default function ViewBatchTransaction({ title }) {
                     <Grid item xs={12} sm={6}>
                         <RenderField label="Status" value={response?.data?.status} />
                     </Grid>
+                    <Grid item xs={12} sm={6}>
+                        <RenderField
+                            label="Total Sent Amount"
+                            value={`${response?.data?.sending_amount} ${response?.data?.sending_currency}`}
+                        />
+                    </Grid>
+
                     <Grid item xs={12} sm={6}>
                         <RenderField label="Service Charge" value={response?.data?.service_charge} />
                     </Grid>
