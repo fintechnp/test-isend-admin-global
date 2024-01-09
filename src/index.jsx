4296416349e4d8dd5b1e55059a7a2d16f6f5d5de
Milan Tarami <milantarami.dev@gmail.com>
2024-01-09 11:11:07 +0545

    style: new loadin screen

--- src/index.jsx
+++ src/index.jsx
@@ -1,42 +1,10 @@
-import React, { Suspense } from "react";
-import { lazy } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
+
 import "./index.css";
+import App from './App'
 import reportWebVitals from "./reportWebVitals";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import CardMedia from "@mui/material/CardMedia";
-import Logo from "../src/assets/long-logo.svg";
 
-const App = lazy(() => import("./App"));
-
-const Card = styled(Box)(({ theme }) => ({
-  height: "96vh",
-  width: "100%",
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "center",
-}));
-
-function ImageCard() {
-  return (
-    <Card>
-      <CardMedia
-        component="img"
-        height="108"
-        image={Logo}
-        alt="document image"
-        sx={{ width: "124px" }}
-      />
-    </Card>
-  );
-}
-
-ReactDOM.render(
-  <Suspense fallback={<ImageCard />}>
-    <App />
-  </Suspense>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 reportWebVitals();
