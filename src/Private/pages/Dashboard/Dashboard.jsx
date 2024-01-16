ddfd83356614f773631b25765ea024a9f13643a0
Milan Tarami <milantarami.dev@gmail.com>
2024-01-16 13:47:03 +0545

    fix: refresh token

--- src/Private/pages/Dashboard/Dashboard.jsx
+++ src/Private/pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,25 @@
 import PageContent from "App/components/Container/PageContent";
+import AuthUtility from "App/utils/AuthUtility";
+import { refreshToken } from "Common/store/sagas";
 import React from "react";
+import { useDispatch } from "react-redux";
 
 function Dashboard() {
+
+  const dispatch = useDispatch()
+
     return (
        <PageContent documentTitle="Dashboard">
          <div>Welcome to iSend</div>
+         <button onClick={() => {
+          dispatch({
+            type: "REFRESH_TOKEN",
+            data: {
+                access_token: AuthUtility.getAccessToken(),
+                refresh_token: AuthUtility.getRefreshToken(),
+            },
+        })
+         }}>sddfa</button>
        </PageContent>
     );
 }
