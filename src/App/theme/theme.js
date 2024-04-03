a438009137f2acea11e9d36ba840ea02dc129a50
Milan Tarami <milantarami.dev@gmail.com>
2024-04-03 11:06:33 +0545

    style: mui global theme setup

--- src/App/theme/theme.js
+++ src/App/theme/theme.js
@@ -2,8 +2,29 @@ import { default as DarkPalette } from "./dark";
 import { default as LightPalette } from "./light";
 import { createTheme } from "@mui/material/styles";
 
+export const fonts = {
+    primary: "Poppins",
+    secondary: "Poppins",
+};
+
+export const inputBorderRadius = {
+    outer: "8px",
+    inner: "4px",
+};
+
+export const buttonBorderRadius = {
+    outer: "4px",
+};
+
 export const ChangeTheme = (mode = true) => {
     const theme = createTheme({
+        typography: {
+            fontFamily: '"Poppins", sans-serif',
+            button: {
+                textTransform: "none",
+            },
+        },
+
         components: {
             MuiCssBaseline: {
                 styleOverrides: `
@@ -22,6 +43,221 @@ export const ChangeTheme = (mode = true) => {
                                 lineHeight: 1.42857,
                             },
                         },`,
+            },
+            MuiTypography: {
+                variants: [
+                    {
+                        props: { variant: "h1" },
+                        style: { fontFamily: fonts.primary },
+                    },
+                    {
+                        props: { variant: "h2" },
+                        style: { fontFamily: fonts.primary },
+                    },
+                    {
+                        props: { variant: "h3" },
+                        style: { fontFamily: fonts.primary },
+                    },
+                    {
+                        props: { variant: "h4" },
+                        style: { fontFamily: fonts.primary },
+                    },
+                    {
+                        props: { variant: "h5" },
+                        style: { fontFamily: fonts.primary },
+                    },
+                    {
+                        props: { variant: "h6" },
+                        style: { fontFamily: fonts.primary },
+                    },
+                    {
+                        props: { variant: "subtitle1" },
+                        style: { fontFamily: fonts.secondary },
+                    },
+                    {
+                        props: { variant: "subtitle2" },
+                        style: { fontFamily: fonts.secondary },
+                    },
+                    {
+                        props: { variant: "body1" },
+                        style: { fontFamily: fonts.secondary },
+                    },
+                    {
+                        props: { variant: "body2" },
+                        style: { fontFamily: fonts.secondary },
+                    },
+                    {
+                        props: { variant: "caption" },
+                        style: { fontFamily: fonts.secondary },
+                    },
+                ],
+                styleOverrides: {
+                    root: {},
+                },
+            },
+            MuiButtonBase: {
+                styleOverrides: {
+                    root: {
+                        "*": {
+                            fontFamily: fonts.secondary,
+                        },
+                    },
+                },
+            },
+            MuiButton: {
+                defaultProps: {
+                    disableElevation: true,
+                },
+                variants: [
+                    {
+                        props: {
+                            size: "large",
+                        },
+                        style: {
+                            padding: "14px 32px 14px 32px",
+                            fontSize: "1rem",
+                            lineHeight: "1.5rem",
+                        },
+                    },
+                ],
+                styleOverrides: {
+                    root: ({ ownerState, theme }) => ({
+                        borderRadius: buttonBorderRadius.outer,
+                    }),
+                },
+            },
+            MuiCssBaseline: {
+                styleOverrides: (theme) => ({
+                    "& .Mui-disabled": {
+                        cursor: "not-allowed",
+                        "& *": {
+                            cursor: "not-allowed !important",
+                        },
+                    },
+                }),
+            },
+            MuiIconButton: {
+                styleOverrides: {
+                    root: {
+                        "&:disabled": {
+                            cursor: "not-allowed",
+                            pointerEvents: "all !important",
+                        },
+                    },
+                },
+            },
+            MuiDivider: {
+                styleOverrides: {
+                    root: ({ ownerState, theme }) => ({
+                        ...ownerState,
+                        borderColor: theme.palette.grey[300],
+                    }),
+                },
+            },
+            MuiTooltip: {
+                styleOverrides: {
+                    tooltip: {
+                        fontSize: "1rem",
+                    },
+                },
+            },
+            MuiFormLabel: {
+                styleOverrides: {
+                    root: {
+                        fontFamily: fonts.secondary,
+                    },
+                },
+            },
+            MuiTextField: {
+                styleOverrides: {
+                    root: ({ theme }) => ({
+                        fontFamily: fonts.secondary,
+                        "&:hover fieldset": {
+                            border: `1px solid ${theme.palette.secondary.main} !important`,
+                        },
+                        "&:focus-within fieldset, &:focus-visible fieldset": {
+                            border: `1px solid ${theme.palette.secondary.main} !important`,
+                        },
+                    }),
+                },
+            },
+            MuiOutlinedInput: {
+                styleOverrides: {
+                    root: ({ theme }) => ({
+                        borderRadius: inputBorderRadius.outer,
+                        fontFamily: fonts.secondary,
+                        "&:hover fieldset": {
+                            border: `1px solid ${theme.palette.secondary.main} !important`,
+                        },
+                        "&:focus-within fieldset, &:focus-visible fieldset": {
+                            border: `1px solid ${theme.palette.secondary.main} !important`,
+                        },
+                    }),
+                },
+            },
+            MuiSelect: {
+                styleOverrides: {
+                    outlined: ({ theme }) => ({
+                        borderRadius: inputBorderRadius.outer,
+                        "&:hover fieldset": {
+                            border: `1px solid ${theme.palette.secondary.main} !important`,
+                        },
+                        "&:focus-within fieldset, &:focus-visible fieldset": {
+                            border: `1px solid ${theme.palette.secondary.main} !important`,
+                        },
+                    }),
+                },
+            },
+            MuiFormHelperText: {
+                styleOverrides: {
+                    root: {
+                        fontFamily: fonts.secondary,
+                    },
+                },
+            },
+            MuiTableHead: {
+                styleOverrides: {
+                    root: {
+                        "& .MuiTableCell-root,.MuiTypography-root": {
+                            fontFamily: fonts.secondary,
+                            lineHeight: "1.5rem",
+                            "& .MuiTypography-root": {
+                                fontFamily: fonts.secondary,
+                                lineHeight: "1.5rem",
+                            },
+                        },
+                    },
+                },
+            },
+            MuiTableBody: {
+                styleOverrides: {
+                    root: {
+                        "& .MuiTableCell-root,.MuiTypography-root": {
+                            fontFamily: fonts.secondary,
+                            lineHeight: "1.5rem",
+                            "& .MuiTypography-root": {
+                                fontFamily: fonts.secondary,
+                                lineHeight: "1.5rem",
+                            },
+                        },
+                    },
+                },
+            },
+            MuiMenuItem: {
+                styleOverrides: {
+                    root: {
+                        fontFamily: fonts.secondary,
+                    },
+                },
+            },
+            MuiListItem: {
+                styleOverrides: {
+                    root: {
+                        "& .MuiTypography-root": {
+                            fontFamily: fonts.secondary,
+                        },
+                    },
+                },
             },
         },
         overrides: {
@@ -51,11 +287,11 @@ export const ChangeTheme = (mode = true) => {
                 },
                 styleOverrides: {
                     root: {
-                        textTransform: 'inherit'
-                    }
-                }
+                        textTransform: "inherit",
+                    },
+                },
             },
-        }
+        },
     });
 
     return theme;
