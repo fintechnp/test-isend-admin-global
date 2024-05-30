60d6b966acb4efd7dc1f9badbcb04f9a9ecb5c37
milanr22 <milanrawal@fintech.com.np>
2024-05-30 11:11:41 +0545

    feat: transaction status added in dropdown

--- src/Private/components/b2b-transactions/SingleTransactionFilterForm.jsx
+++ src/Private/components/b2b-transactions/SingleTransactionFilterForm.jsx
@@ -15,6 +15,7 @@ import FormSearchAutoComplete from "App/core/hook-form/FormSearchAutocomplete";
 import isEmpty from "App/helpers/isEmpty";
 import apiEndpoints from "Private/config/apiEndpoints";
 import { yupResolver } from "@hookform/resolvers/yup";
+import referenceTypeId from "Private/config/referenceTypeId";
 
 const formValidationSchema = Yup.object().shape({
     from_date: Yup.string()
@@ -31,7 +32,7 @@ const formValidationSchema = Yup.object().shape({
                 return isEmpty(context.parent.to_date) && !isEmpty(value);
             },
         }),
-        to_date: Yup.string()
+    to_date: Yup.string()
         .nullable()
         .test({
             name: "required_when_from_date_is_not_empty",
@@ -44,10 +45,12 @@ const formValidationSchema = Yup.object().shape({
                     return true;
                 return isEmpty(context.parent.from_date) && !isEmpty(value);
             },
-        })
+        }),
 });
 
 export default function SingleTransactionFilterForm({ onSubmit, onReset, loading }) {
+    const reference = JSON.parse(localStorage.getItem("reference"));
+
     const methods = useForm({
         defaultValues: {
             sort_by: "created_ts",
@@ -100,7 +103,19 @@ export default function SingleTransactionFilterForm({ onSubmit, onReset, loading
                     <FormTextField name="txn_no" label="Transaction No" />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
-                    <FormSelect name="status" label="Status" options={[]} />
+                    <FormSelect
+                        name="status"
+                        label="Status"
+                        options={
+                            reference &&
+                            reference
+                                ?.filter((ref_data) => ref_data.reference_type === referenceTypeId.transactionStatus)[0]
+                                .reference_data.map((ref) => ({
+                                    label: ref.name,
+                                    value: ref.value,
+                                }))
+                        }
+                    />
                 </Grid>
                 <Grid item xs={12} sm={6} md={4}>
                     <FormDatePicker name="from_date" label="From Date" />
