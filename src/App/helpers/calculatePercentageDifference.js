83168270e6d32e6e89a87ba8cc7578020936119b
Milan Tarami <milantarami.dev@gmail.com>
2024-06-11 12:43:10 +0545

    fix: percentage value

--- src/App/helpers/calculatePercentageDifference.js
+++ src/App/helpers/calculatePercentageDifference.js
@@ -3,19 +3,21 @@
  *
  * @param {number} currentCount - The current count value.
  * @param {number} previousCount - The previous count value.
- * @returns {number} - The percentage difference.
+ * @returns {number} - The percentage difference, capped at 100%.
  */
 export default function calculatePercentageDifference(currentCount, previousCount) {
-    if (previousCount === 0 && currentCount === 0) {
-        return 0;
-    } else if (previousCount === 0) {
-        return Infinity;
-    } else if (currentCount === 0) {
-        return -100;
+    if (previousCount === 0) {
+        if (currentCount === 0) {
+            return 0;
+        }
+        return 100;
     }
-
+    
     const difference = currentCount - previousCount;
     const percentageDifference = (difference / previousCount) * 100;
+    
+    // Cap the percentage difference at 100%
+    const cappedPercentageDifference = Math.min(percentageDifference, 100);
 
-    return parseFloat(percentageDifference.toFixed(2));
+    return parseFloat(cappedPercentageDifference.toFixed(2));
 }
