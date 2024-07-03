ed4aa8daded3736ffbb1c79f14f7d67d4451c52e
Milan Tarami <milantarami.dev@gmail.com>
2024-07-03 15:13:56 +0545

    fix: beneficiary form

--- src/Private/pages/Customers/Beneficiary/AddUpdateBeneficiary/Form/BasicForm.jsx
+++ src/Private/pages/Customers/Beneficiary/AddUpdateBeneficiary/Form/BasicForm.jsx
@@ -3,7 +3,6 @@ import Button from "@mui/material/Button";
 import { useDispatch } from "react-redux";
 import Divider from "@mui/material/Divider";
 import { styled } from "@mui/material/styles";
-import Typography from "@mui/material/Typography";
 import React, { useState, useEffect } from "react";
 import { Field, Form, reduxForm } from "redux-form";
 import TextField from "App/components/Fields/TextField";
@@ -58,7 +57,6 @@ const NextButton = styled(Button)(({ theme }) => ({
 const Basic = ({ handleSubmit, handleBack, activeStep, steps, buttonText, update, pcountry, phone_code }) => {
     const dispatch = useDispatch();
 
-
     const reference = JSON.parse(localStorage.getItem("reference"));
 
     const [filterSchema, setFilterSchema] = useState({
@@ -68,12 +66,6 @@ const Basic = ({ handleSubmit, handleBack, activeStep, steps, buttonText, update
         sort_by: "created_ts",
         order_by: "DESC",
     });
-
-    useEffect(() => {
-        if (phone_code) {
-            setCode(phone_code);
-        }
-    }, [phone_code]);
 
     useEffect(() => {
         if (pcountry) {
@@ -150,7 +142,6 @@ const Basic = ({ handleSubmit, handleBack, activeStep, steps, buttonText, update
                                 validate={Validator.maxLength50}
                             />
                         </FieldWrapper>
-
                     </FormWrapper>
                 </Grid>
                 <Grid item>
