81ec64840e588096de4ca2dad508b7b7a3dea06f
Milan Tarami <milantarami.dev@gmail.com>
2024-07-16 13:21:46 +0545

    fix: fn undefined in useCountries hooks

--- src/App/hooks/useCountries.js
+++ src/App/hooks/useCountries.js
@@ -1,33 +1,81 @@
+import { localStorageGet } from "App/helpers/localStorage";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import isEmpty from "App/helpers/isEmpty";
+/**
+ * @typedef {object} Country
+ * @property {number} country_id
+ * @property {number} tid
+ * @property {string} country
+ * @property {string} currency
+ * @property {string} currency_name
+ * @property {string} iso2
+ * @property {string} iso3
+ * @property {string} phone_code
+ * @property {null} phone_regex
+ * @property {null} postcode_regex
+ * @property {boolean} has_state
+ * @property {number} id
+ * @property {string} created_ts
+ * @property {null} created_by
+ * @property {string} updated_ts
+ * @property {null} updated_by
+ */
 
+/**
+ * @typedef {object} UseCountriesReturn
+ * @property {Country[]} countries
+ * @property {(countryId: number) => Country|null} getCountryById,
+ * @property {(iso3: string) => Country|null} getCountryByIso3
+ * @property {boolean} isLoading,
+ * @property {(currency: string) => Country|null} getCurrencyName
+*/
+
+/**
+ * @returns {UseCountriesReturn}
+*/
 const useCountries = () => {
-    const dispatch = useDispatch();
-
     const { response, loading: isLoading } = useSelector((state) => state.all_country);
 
-    const countries = response.data ?? [];
+    const countries = response?.data ?? localStorageGet("country") ?? [];
 
-    const getCountryNameByISO3 = (iso3) => {
-        if (isEmpty(iso3)) throw new Error("iso3 code is required");
-        return countries.find((d) => d.iso3.toUpperCase() === iso3.toUpperCase())?.country ?? null;
+    const dispatch = useDispatch();
+
+    /**
+     * get country by id
+     *
+     * @param {int} countryId
+     * @returns {Country|null}
+     */
+    const getCountryById = (countryId) => {
+        return countries.find((c) => Number(c.country_id) === Number(countryId)) ?? null;
     };
 
-    const fetch = () => {
-        dispatch({
-            type: "GET_ALL_COUNTRY",
-        });
+    /**
+     * get country by id
+     *
+     * @param {int} countryId
+     * @returns {Country|null}
+     */
+    const getCountryByIso3 = (iso3) => {
+        return countries.find((c) => c.iso3.toUpperCase() === countryId?.toUpperCase()) ?? null;
+    };
+
+    /**
+     * get currency name
+     *
+     * @param {string} currency
+     * @returns {Country|undefined}
+     */
+    const getCurrencyName = (currency) => {
+        return countries.find((ct) => ct.currency.toUpperCase() === currency?.toUpperCase())?.currency_name ?? null;
     };
 
     useEffect(() => {
-        if (countries.length <= 0) {
-            fetch();
-        }
+        if (countries.length <= 0) dispatch({ type: "GET_COUNTRIES" });
     }, []);
 
-    return { countries, fetch, getCountryNameByISO3, isLoading };
+    return { countries, getCountryById, getCountryByIso3, isLoading, getCurrencyName };
 };
 
 export default useCountries;
