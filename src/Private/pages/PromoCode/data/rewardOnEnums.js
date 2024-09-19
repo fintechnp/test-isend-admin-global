8b794527a7689d50bbfd47f8304eacd7198c429e
milanr22 <139739333+milanr22@users.noreply.github.com>
2024-09-19 13:53:07 +0545

    feat: incentive drop down added (#314)

--- src/Private/pages/PromoCode/data/rewardOnEnums.js
+++ src/Private/pages/PromoCode/data/rewardOnEnums.js
@@ -1,10 +1,15 @@
 export const rewardOnEnums = {
     SERVICE_CHARGE: 0,
+    INCENTIVE: 1,
 };
 
 export const rewardOnOptions = [
     {
         label: "Service Charge",
         value: rewardOnEnums.SERVICE_CHARGE,
+    },
+    {
+        label: "Incentive",
+        value: rewardOnEnums.INCENTIVE,
     },
 ];
