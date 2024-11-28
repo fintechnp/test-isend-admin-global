4505f062690f8194acc2efcea9b4c969a421acb8
Govind Bohara <govindbohara@Govinds-MacBook-Pro.local>
2024-11-28 11:39:02 +0545

    feat: added new switcg

--- src/Private/pages/Dashboard/components/CurrencyData.jsx
+++ src/Private/pages/Dashboard/components/CurrencyData.jsx
@@ -62,6 +62,55 @@ const BoldText = styled(Typography)(({ theme }) => ({
     fontSize: 16,
 }));
 
+const CustomSwitch = styled((props) => <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />)(
+    ({ theme }) => ({
+        width: 30,
+        height: 18,
+        padding: 0,
+
+        "& .MuiSwitch-switchBase": {
+            padding: 0,
+            margin: "4px",
+            transitionDuration: "300ms",
+            "&.Mui-checked": {
+                transform: "translateX(12px)",
+                color: "#fff",
+                "& + .MuiSwitch-track": {
+                    backgroundColor: theme.palette.primary.main,
+                    opacity: 1,
+                    border: 0,
+                },
+                "&.Mui-disabled + .MuiSwitch-track": {
+                    opacity: 0.5,
+                },
+            },
+            "&.Mui-focusVisible .MuiSwitch-thumb": {
+                border: "6px solid #fff",
+            },
+            "&.Mui-disabled .MuiSwitch-thumb": {
+                color: theme.palette.mode === "light" ? theme.palette.grey[100] : theme.palette.grey[600],
+            },
+            "&.Mui-disabled + .MuiSwitch-track": {
+                opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+            },
+        },
+        "& .MuiSwitch-thumb": {
+            boxSizing: "border-box",
+            width: 10,
+            height: 10,
+        },
+
+        "& .MuiSwitch-track": {
+            borderRadius: 32 / 2,
+            backgroundColor: "#C1C3C7",
+            opacity: 1,
+            transition: theme.transitions.create(["background-color"], {
+                duration: 500,
+            }),
+        },
+    }),
+);
+
 export default function DashboardCurrencyData() {
     const todayDate = dateUtils.getFormattedDate(new Date());
     return (
@@ -75,9 +124,9 @@ export default function DashboardCurrencyData() {
                     })}
                 >
                     <Typography fontWeight={600}>As of {todayDate}</Typography>
-                    <Row alignItems="center">
+                    <Row alignItems="center" gap={1}>
                         <Typography fontSize={16}>SELL</Typography>
-                        <Switch />
+                        <CustomSwitch />
                         <Typography fontSize={16}>BUY</Typography>
                     </Row>
                 </Column>
