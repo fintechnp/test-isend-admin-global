8d7a7b90b5bd749515fd112e6b72316b5407dc34
thambdrpun <thambahadur.pun@fintech.com.np>
2024-12-10 09:09:14 +0545

    fix: import

--- src/Private/pages/Setup/CampaignAttribute/ListCampaignAttribute.jsx
+++ src/Private/pages/Setup/CampaignAttribute/ListCampaignAttribute.jsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import Typography from "@mui/material/Typography";
 import React, { useCallback, useEffect, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import ListItemButton from "@mui/material/ListItemButton";
